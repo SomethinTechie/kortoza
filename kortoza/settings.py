d73c157ac72eeeda730f101262a5b053bb84e1b4
SomethinTechie <phokwanemaria@gmail.com>
2022-10-06 21:21:58 +0200

    static

--- kortoza/settings.py
+++ kortoza/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
